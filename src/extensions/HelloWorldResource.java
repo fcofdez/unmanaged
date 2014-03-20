022253d58e5f6304575102e08418ba6e19c38e8f
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2014-03-20 01:38:14 +0100

    Second example.

--- src/extensions/HelloWorldResource.java
+++ src/extensions/HelloWorldResource.java
@@ -3,21 +3,25 @@ package extensions; /**
  */
 import java.nio.charset.Charset;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.*;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.*;
+
 
 //START SNIPPET: extensions.HelloWorldResource
 @Path( "/helloworld" )
 public class HelloWorldResource
 {
+    private static enum RelTypes implements RelationshipType
+    {
+        KNOWS
+    }
+
     private final GraphDatabaseService database;
 
     public HelloWorldResource( @Context GraphDatabaseService database )
@@ -30,8 +34,38 @@ public class HelloWorldResource
     @Path( "/{nodeId}" )
     public Response hello( @PathParam( "nodeId" ) long nodeId )
     {
-        // Do stuff with the database
+
         return Response.status( Status.OK ).entity(
                 ("Hello World, nodeId=" + nodeId).getBytes( Charset.forName("UTF-8") ) ).build();
     }
+
+    @Path("/{nodeId}")
+    @POST
+    public Response createUser( @PathParam("nodeId") long nodeId,
+                                @DefaultValue("a") @QueryParam("nodea") String nodea,
+                                @DefaultValue("b") @QueryParam("nodeb") String nodeb)
+    {
+        try ( Transaction tx = this.database.beginTx() )
+        {
+
+            Node firstNode, secondNode;
+            Relationship relationship;
+            Label msg = DynamicLabel.label("MESSAGE");
+
+
+            firstNode = this.database.createNode();
+            firstNode.setProperty( "message", nodea );
+            firstNode.addLabel(msg);
+            secondNode = this.database.createNode();
+            secondNode.addLabel(msg);
+            secondNode.setProperty( "message", nodeb );
+
+            relationship = firstNode.createRelationshipTo( secondNode, RelTypes.KNOWS );
+            relationship.setProperty( "message", "brave Neo4j " );
+
+            tx.success();
+        }
+        return Response.status( Status.OK ).build();
+    }
+
 }
